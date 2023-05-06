2be88756d4dfe284b9862a53ecc05f02077dd4f8
Marcos <marcoswodbarros@outlook.com>
2023-05-06 03:55:03 -0300

    Adjusting mobile positions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('C:/Users/Marcos/OneDrive/Documentos/Programação/Freela/1- Ibra/ibra-tenis-de-mesa/src/images/raquetes.jpg')"
+      },  
+
+      fontFamily: {
+        barlow: 'Barlow Condensed',
+        roboto: 'Roboto Condensed'
+      },
+    },
   },
   plugins: [],
 }
