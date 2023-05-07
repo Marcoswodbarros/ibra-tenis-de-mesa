d509dc9517bc342c4117fd77b353f730346a2e91
Marcos <marcoswodbarros@outlook.com>
2023-05-07 00:56:47 -0300

    Updating js and apply

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,18 @@
 let menuIcon = document.querySelector('#header__icons--menu');
 let arrowIcon = document.querySelector('#header__icons--arrow');
 let nav = document.querySelector('nav');
+let mainButton = document.querySelector('#main__button');
 
 menuIcon.addEventListener("click", function () {
     menuIcon.style.display = "none";
     arrowIcon.style.display = "block";
     nav.style.display = "block";
+    mainButton.style.display = "none";
 })
 
 arrowIcon.addEventListener("click", function () {
     menuIcon.style.display = "block";
     arrowIcon.style.display = "none";
     nav.style.display = "none";
+    mainButton.style.display = "flex";
 })
