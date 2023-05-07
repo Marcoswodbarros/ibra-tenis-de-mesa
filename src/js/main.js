e9399881661a364825458908455754e955892177
Marcos <marcoswodbarros@outlook.com>
2023-05-07 00:15:21 -0300

    Updating navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,15 @@
 let menuIcon = document.querySelector('#header__icons--menu');
 let arrowIcon = document.querySelector('#header__icons--arrow');
+let nav = document.querySelector('nav');
 
 menuIcon.addEventListener("click", function () {
     menuIcon.style.display = "none";
     arrowIcon.style.display = "block";
+    nav.style.display = "block";
 })
 
 arrowIcon.addEventListener("click", function () {
     menuIcon.style.display = "block";
     arrowIcon.style.display = "none";
+    nav.style.display = "none";
 })
