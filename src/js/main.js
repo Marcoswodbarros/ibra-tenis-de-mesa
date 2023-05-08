11544d362b64893302a096007a3527f9303a1613
Marcos <marcoswodbarros@outlook.com>
2023-05-08 16:48:45 -0300

    Refactoring js

--- src/js/main.js
+++ src/js/main.js
@@ -5,32 +5,15 @@ let lg = window.matchMedia("(min-width:1024px)");
 
 menuIcon.addEventListener("click", menuOff);
 arrowIcon.addEventListener("click", menuOn);
-lg.addEventListener("change", menuOff, menuOn);
 
-function menuOff () {
-    if (window.matchMedia("(min-width:1024px)").matches) {
-        menuIcon.style.display = "none";
-        arrowIcon.style.display = "none";
-        nav.style.display = "block";
-    }
-
-    else {
-        menuIcon.style.display = "none";
-        arrowIcon.style.display = "block";
-        nav.style.display = "block";
-    } 
+function menuOff() {
+    menuIcon.style.display = "none";
+    arrowIcon.style.display = "block";
+    nav.style.display = "block";
 }
 
-function menuOn () {
-    if (window.matchMedia("(min-width:1024px)").matches) {
-        menuIcon.style.display = "none";
-        arrowIcon.style.display = "none";
-        nav.style.display = "block";
-    }
-
-    else {
-        menuIcon.style.display = "block";
-        arrowIcon.style.display = "none";
-        nav.style.display = "none";
-    }
+function menuOn() {
+    menuIcon.style.display = "block";
+    arrowIcon.style.display = "none";
+    nav.style.display = "none";
 }
