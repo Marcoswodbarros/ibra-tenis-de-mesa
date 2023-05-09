fd1f1efa118fc299782ffdccbc8c296d45c7a40c
Marcos <marcoswodbarros@outlook.com>
2023-05-09 18:06:50 -0300

    Finishing project

--- src/js/circuito.js
+++ src/js/circuito.js
@@ -1,8 +1,5 @@
 let menuIcon = document.querySelector('#header__icons--menu');
 let arrowIcon = document.querySelector('#header__icons--arrow');
-let nav = document.querySelector('nav');
-let subtitulo = document.querySelector('#main__subtitulo');
-let titulo = document.querySelector('#main__titulo');
 let circuitoButton = document.querySelector('#circuito__main--link');
 
 menuIcon.addEventListener("click", menuOff);
@@ -11,15 +8,11 @@ arrowIcon.addEventListener("click", menuOn);
 function menuOff() {
     menuIcon.style.display = "none";
     arrowIcon.style.display = "block";
-    nav.style.display = "block";
-    subtitulo.style.display = "none";
     titulo.style.display = "none";
 }
 
 function menuOn() {
     menuIcon.style.display = "block";
     arrowIcon.style.display = "none";
-    nav.style.display = "none";
-    subtitulo.style.display = "block";
-    titulo.style.display = "block";
+    titulo.style.display = "none";
 }
