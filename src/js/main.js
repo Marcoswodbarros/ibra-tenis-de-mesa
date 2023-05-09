9227d0f247d2116fa40a08b260392e558a73ec58
Marcos <marcoswodbarros@outlook.com>
2023-05-09 11:42:25 -0300

    Updating title

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 let menuIcon = document.querySelector('#header__icons--menu');
 let arrowIcon = document.querySelector('#header__icons--arrow');
 let nav = document.querySelector('nav');
+let subtitulo = document.querySelector('#main__subtitulo');
 let titulo = document.querySelector('#main__titulo');
 
 menuIcon.addEventListener("click", menuOff);
@@ -10,6 +11,7 @@ function menuOff() {
     menuIcon.style.display = "none";
     arrowIcon.style.display = "block";
     nav.style.display = "block";
+    subtitulo.style.display = "none";
     titulo.style.display = "none";
 }
 
@@ -17,5 +19,6 @@ function menuOn() {
     menuIcon.style.display = "block";
     arrowIcon.style.display = "none";
     nav.style.display = "none";
+    subtitulo.style.display = "block";
     titulo.style.display = "block";
 }
