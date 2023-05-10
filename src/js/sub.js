5aee34839f7d719fa3271014fd1bd46c5e683ad8
Marcos <marcoswodbarros@outlook.com>
2023-05-10 12:14:56 -0300

    Refactoring circuito button

---
+++ src/js/sub.js
@@ -0,0 +1,18 @@
+let menuIcon = document.querySelector('#header__icons--menu');
+let arrowIcon = document.querySelector('#header__icons--arrow');
+let nav = document.querySelector('nav');
+
+menuIcon.addEventListener("click", menuOff);
+arrowIcon.addEventListener("click", menuOn);
+
+function menuOff() {
+    menuIcon.style.display = "none";
+    arrowIcon.style.display = "block";
+    nav.style.display = "block";
+}
+
+function menuOn() {
+    menuIcon.style.display = "block";
+    arrowIcon.style.display = "none";
+    nav.style.display = "none";
+}
