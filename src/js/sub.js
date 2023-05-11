0a79c3d735dd7c3e9b2e36864f6234b5fab6366e
Marcos <marcoswodbarros@outlook.com>
2023-05-11 14:34:21 -0300

    Refactoring nav

--- src/js/sub.js
+++ src/js/sub.js
@@ -2,6 +2,7 @@ let menuIcon = document.querySelector('#header__icons--menu');
 let arrowIcon = document.querySelector('#header__icons--arrow');
 let nav = document.querySelector('nav');
 let main = document.querySelector('main');
+let body = document.querySelector('body');
 
 menuIcon.addEventListener("click", menuOff);
 arrowIcon.addEventListener("click", menuOn);
@@ -11,6 +12,7 @@ function menuOff() {
     arrowIcon.style.display = "block";
     nav.style.display = "block";
     main.style.display = "none";
+    // body.style.backgroundColor = "rgb(71, 85, 105)";
 }
 
 function menuOn() {
@@ -18,4 +20,5 @@ function menuOn() {
     arrowIcon.style.display = "none";
     nav.style.display = "none";
     main.style.display = "flex";
+    // body.style.backgroundColor = "rgb(7, 10, 82)";
 }
