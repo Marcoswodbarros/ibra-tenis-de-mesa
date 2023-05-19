3b4675bba91c8165b9fbacace591caab208a3bde
Marcos <marcoswodbarros@outlook.com>
2023-05-19 00:59:23 -0300

    Mobile: updating nav size

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 let menuIcon = document.querySelector('#header__icons--menu');
 let arrowIcon = document.querySelector('#header__icons--arrow');
 let nav = document.querySelector('nav');
+let main = document.querySelector('#main__intro');
 let titulo = document.querySelector('#main__titulo');
 let circuitoButton = document.querySelector('#circuito__main--link');
+let patrocinadores = document.querySelector('#main__patrocinadores');
 
 menuIcon.addEventListener("click", menuOff);
 arrowIcon.addEventListener("click", menuOn);
@@ -11,10 +13,14 @@ function menuOff() {
     menuIcon.style.display = "none";
     arrowIcon.style.display = "block";
     nav.style.display = "block";
+    main.style.display = "none";
+    patrocinadores.style.display = "none";
 }
 
 function menuOn() {
     menuIcon.style.display = "block";
     arrowIcon.style.display = "none";
     nav.style.display = "none";
+    main.style.display = "flex";
+    patrocinadores.style.display = "flex";
 }
